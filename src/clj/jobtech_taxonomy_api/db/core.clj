57ba303752c48109341423e4bf0abdf7019f99fc
qeshi <6067421+qeshi@users.noreply.github.com>
2019-02-26 13:17:37 +0100

    reverting back to using env to get the database-name

--- src/clj/jobtech_taxonomy_api/db/core.clj
+++ src/clj/jobtech_taxonomy_api/db/core.clj
@@ -17,30 +17,10 @@
   :start (d/connect (get-client)  {:db-name (:datomic-name env)}))
 
 
-
-(def cfg { :datomic-name "taxonomy_v13"
-          :datomic-cfg {
-                        :server-type :ion
-                        :region "eu-west-1" ;; e.g. us-east-1
-                        :system "prod-jobtech-taxonomy-db"
-                        ;;:creds-profile "<your_aws_profile_if_not_using_the_default>"
-                        :endpoint "http://entry.prod-jobtech-taxonomy-db.eu-west-1.datomic.net:8182/"
-                        :proxy-port 8182}
-
-          })
-
-;; (defn get-client [] (d/client (:datomic-cfg cfg)))
-
-
-
 (defn get-db [] (d/db conn))
 
 (defn get-conn "" []
   (d/connect (get-client)  {:db-name (:datomic-name env)}))
-
-(defn get-conn "" []
-(d/connect (get-client)  {:db-name "taxonomy_v13"}))
-
 
 
 (def find-concept-by-preferred-term-query
