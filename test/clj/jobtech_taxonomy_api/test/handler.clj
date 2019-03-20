47e592ab576989693dddb2ae7300be58d4b748e5
qeshi <6067421+qeshi@users.noreply.github.com>
2019-03-20 18:15:04 +0100

    added datomic dependecies

--- test/clj/jobtech_taxonomy_api/test/handler.clj
+++ test/clj/jobtech_taxonomy_api/test/handler.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
             [jobtech-taxonomy-api.handler :refer :all]
+            [jobtech-taxonomy-api.db.core :refer :all]
             [jobtech-taxonomy-api.middleware.formats :as formats]
             [muuntaja.core :as m]
             [mount.core :as mount]))
@@ -12,15 +13,25 @@
 (use-fixtures
   :once
   (fn [f]
-    (mount/start #'jobtech-taxonomy-api.config/env
+    (mount/start #'jobtech-taxonomy-api.db.core/conn
+                 #'jobtech-taxonomy-api.config/env
                  #'jobtech-taxonomy-api.handler/app)
     (f)))
 
 (deftest test-app
-  (testing "main route"
+
+  #_(testing "main route"
     (let [response (app (request :get "/"))]
       (is (= 200 (:status response)))))
 
-  (testing "not-found route"
+  #_(testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))
+
+
+  (testing "full history"
+    (let [response (app (request :get "/taxonomy/public-api/full-history"))]
+      (prn response)
+      (is (= 202 (:status response)))))
+
+  )
