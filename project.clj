409a7c2f61e7e77ac419d7e06d60fb86dd9b4c04
qeshi <6067421+qeshi@users.noreply.github.com>
2019-03-22 10:35:11 +0100

    Added integration test in project.clj

--- project.clj
+++ project.clj
@@ -68,7 +68,8 @@
                                  [pjstadig/humane-test-output "0.9.0"]
                                  [prone "1.6.1"]
                                  [ring/ring-devel "1.7.1"]
-                                 [ring/ring-mock "0.3.2"]]
+                                 [ring/ring-mock "0.3.2"]
+                                 ]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]]
 
                   :source-paths ["env/dev/clj"]
