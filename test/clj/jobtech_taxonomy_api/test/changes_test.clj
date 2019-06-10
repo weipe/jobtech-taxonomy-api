c12083447927a46b23113021e275eb47a9cbe8be
SickanEkman <sara.d.ekman@gmail.com>
2019-06-10 16:56:52 +0200

    - unused import + reformat

--- test/clj/jobtech_taxonomy_api/test/changes_test.clj
+++ test/clj/jobtech_taxonomy_api/test/changes_test.clj
@@ -1,70 +1,40 @@
 (ns jobtech-taxonomy-api.test.changes-test
   (:require [clojure.test :as test]
-            [jobtech-taxonomy-api.test.test-utils :as util]
-            [jobtech-taxonomy-api.db.events :as events]
-            ))
+            [jobtech-taxonomy-api.test.test-utils :as util]))
 
 (def run-fixture? (atom true))
 
-
 (defn my-filtering-created-function [element]
-
-  (= "CREATED" (:eventType element))
-  )
-
+  (= "CREATED" (:eventType element)))
 
 (defn my-filtering-transactionid-function [element]
-
-  (= "13194139533328" (:transactionId element))
-  )
+  (= "13194139533328" (:transactionId element)))
 
 (defn my-filtering-deprecated-function [element]
-
-  (= "DEPRECATED" (:eventType element))
-  )
+  (= "DEPRECATED" (:eventType element)))
 
 (test/use-fixtures :each (partial util/fixture run-fixture?))
 
 (test/deftest ^:integration changes-test-0
   (test/testing "test event stream created"
     (let [[status body] (util/send-request-to-json-service
-                          :get "/v0/taxonomy/public/changes"
-                          :headers [util/header-auth-user]
-                          :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])
-          ]
-      (test/is (not (empty (filter my-filtering-created-function body))))
-      )))
+                         :get "/v0/taxonomy/public/changes"
+                         :headers [util/header-auth-user]
+                         :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])]
+      (test/is (not-empty (filter my-filtering-created-function body))))))
 
 (test/deftest ^:integration changes-test-1
   (test/testing "test event stream deprecated"
     (let [[status body] (util/send-request-to-json-service
-                          :get "/v0/taxonomy/public/changes"
-                          :headers [util/header-auth-user]
-                          :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])
-          ]
-      (test/is (not (empty? (filter my-filtering-deprecated-function body))))
-      )))
+                         :get "/v0/taxonomy/public/changes"
+                         :headers [util/header-auth-user]
+                         :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])]
+      (test/is (not-empty (filter my-filtering-deprecated-function body))))))
 
 (test/deftest ^:integration changes-test-2
   (test/testing "test event stream transactionid"
     (let [[status body] (util/send-request-to-json-service
-                          :get "/v0/taxonomy/public/changes"
-                          :headers [util/header-auth-user]
-                          :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])
-          ]
-      (test/is (empty? (filter my-filtering-transactionid-function body)))
-      )))
-
-(test/deftest ^:integration changes-test-3
-  (test/testing "test event stream"
-    (let [[status body] (util/send-request-to-json-service
-                          :get "/v0/taxonomy/public/changes"
-                          :headers [util/header-auth-user]
-                          :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])
-          an-event (first body)]
-      (prn an-event)
-      (test/is (= "CREATED" (:eventType an-event))))))
-
-
-
-
+                         :get "/v0/taxonomy/public/changes"
+                         :headers [util/header-auth-user]
+                         :query-params [{:key "fromDateTime", :val "2018-05-21%2009%3A46%3A08"}])]
+      (test/is (empty? (filter my-filtering-transactionid-function body))))))
