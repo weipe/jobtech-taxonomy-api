d39f93916711e6333a5204db2c868abc438facb4
reiul <ulf.reiman@arbetsformedlingen.se>
2019-06-13 08:48:54 +0200

    varje funktion har unikt namn

--- test/clj/jobtech_taxonomy_api/test/auth_test.clj
+++ test/clj/jobtech_taxonomy_api/test/auth_test.clj
@@ -5,33 +5,33 @@
 
 (test/use-fixtures :each util/fixture)
 
-(test/deftest ^:integration-inactive unauthorized-access-public
+(test/deftest ^:integration-unauthorized-access-public unauthorized-access-public
   (test/testing "unauthorized access to /v0/taxonomy/public/concept/types"
     (let [[status body] (util/send-request-to-json-service :get "/v0/taxonomy/public/concept/types")]
       (test/is (and (= "unauthorized" (:error body))
                (= status 401))))))
 
-(test/deftest ^:integration-inactive authorized-access-public
+(test/deftest ^:integration-authorized-access-public authorized-access-public
   (test/testing "unauthorized access to /v0/taxonomy/public/concept/types"
     (let [[status body] (util/send-request-to-json-service
                          :get "/v0/taxonomy/public/concept/types"
                          :headers [util/header-auth-user])]
       (test/is (= status 200)))))
 
-(test/deftest ^:integration-inactive unauthorized-access-private
+(test/deftest ^:integration-unauthorized-access-private unauthorized-access-private
   (test/testing "unauthorized access to /v0/taxonomy/private/relation/types"
     (let [[status body] (util/send-request-to-json-service :get "/v0/taxonomy/private/relation/types")]
       (test/is (and (= "unauthorized" (:error body))
                (= status 401))))))
 
-(test/deftest ^:integration-inactive authorized-access-private
+(test/deftest ^:integration-authorized-access-private authorized-access-private
   (test/testing "authorized access to /v0/taxonomy/private/relation/types"
     (let [[status body] (util/send-request-to-json-service
                          :get "/v0/taxonomy/private/relation/types"
                          :headers [util/header-auth-admin])]
       (test/is (= status 200)))))
 
-(test/deftest ^:integration-inactive authenticated-and-unauthorized-access-private
+(test/deftest ^:integration-authenticated-and-unauthorized-access-private authenticated-and-unauthorized-access-private
   (test/testing "authenticated and unauthorized access to /v0/taxonomy/private/relation/types"
     (let [[status body] (util/send-request-to-json-service
                          :get "/v0/taxonomy/private/relation/types"
